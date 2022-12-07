47e8225d8e6134d8b7e600ffd1d38317d165f2b5
Lorenco Waleka <lorenco.waleka96@gmail.com>
2022-12-07 18:32:43 +0100

    4.3

---
+++ src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -0,0 +1,28 @@
+package com.kodilla.collections.lists.homework;
+
+import com.kodilla.collections.arrays.homework.CarUtils;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Opel;
+import com.kodilla.collections.interfaces.homework.Bmw;
+import com.kodilla.collections.interfaces.homework.Mazda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarsListApplication {
+    public static void main(String[] args) {
+        List<Car> cars = new ArrayList<>();
+        Opel opel = new Opel(10);
+        cars.add(new Bmw(30));
+        cars.add(new Mazda(25));
+        cars.add(opel);
+
+        cars.remove(0);
+        cars.remove(opel);
+
+        System.out.println(cars.size());
+        for (Car car : cars) {
+            CarUtils.describeCar(car);
+        }
+    }
+}
